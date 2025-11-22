8b1d24db5acf7f2dc150228d641b5af4a3fd6fe1
Shrimmp <camfox1213@gmail.com>
2025-11-22 17:46:14 -0600

    made the game run with a black screen at the correct resolution

--- AimTrainer.py
+++ AimTrainer.py
@@ -7,3 +7,14 @@ SCREEN_WIDTH = 1920
 SCREEN_HEIGHT = 1080
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        
+        screen.fill((0,0,0))
+        pygame.display.flip()
+        
